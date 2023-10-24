23d1245bd28c3db143820663debe4085d1b6faa4
root <fabianagtellechea@gmail.com>
2023-10-23 18:11:11 -0700

    try: fixing code error in task 4

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         date = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) > 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, date)
                 setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns string representation of object"""
