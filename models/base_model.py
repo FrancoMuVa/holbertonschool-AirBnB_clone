a8cfa626bb96da6ba45c58963438a40719bbb351
root <fabianagtellechea@gmail.com>
2023-10-23 17:08:44 -0700

    added: first update of basemodel instantiation working

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     """Abstract class"""
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
+
+        date = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, date)
+                setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
